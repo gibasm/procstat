fbd1e7b0dd80e31e9520d912e16714675bc6a422
mbedded-mike <m.gibas2407@gmail.com>
2023-08-01 21:27:37 +0200

    Implement SIGINT handling and safe exiting from the program
    Reader thread now also checks the "read_procstat" return value.
    
    All of the signals are blocked now in the reader thread, as any signals
    should be handled only in context of the main thread.

--- src/main.c
+++ src/main.c
@@ -3,28 +3,51 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define READER_FIFO_LEN 16UL
 
 queue_t raw_pstat_fifo;
 
+static pthread_t reader_thread;
+static reader_args_t reader_args = {
+    .done = false
+};
+
+/**
+ * @brief safely exit the program, wait for all the threads to finish and free any dynamic buffers
+ */
+static void main_exit();
+
 int main(void) {          
     raw_pstat_fifo = queue_init(READER_FIFO_LEN);
 
-    pthread_t reader_thread;  
-    reader_args_t reader_args = {
-        .done = false
-    };
-
     if(pthread_create(&reader_thread, NULL, pstat_reader_start, &reader_args) != 0) {
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    reader_args.done = true;
+    int retval = EXIT_SUCCESS;
 
+    sigset_t sigmask;
+    sigemptyset(&sigmask);
+    sigaddset(&sigmask, SIGINT); 
+    int signum;
+    
+    if(sigwait(&sigmask, &signum) != 0) {
+        retval = EXIT_FAILURE;
+    }
+
+    if(pthread_sigmask(SIG_BLOCK, &sigmask, NULL) != 0) {
+        retval = EXIT_FAILURE;
+    }
+
+    main_exit();
+
+    return retval; 
+}
+
+static void main_exit() {
+    reader_args.done = true;  
     pthread_join(reader_thread, NULL);
-   
-    queue_free(&raw_pstat_fifo);    
-
-    return 0;
+    queue_free(&raw_pstat_fifo);
 }
--- src/reader/reader.c
+++ src/reader/reader.c
@@ -3,15 +3,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <signal.h>
 
 void* pstat_reader_start(void* arg) { 
+    sigset_t sigmask;
+    sigfillset(&sigmask);
+    if(pthread_sigmask(SIG_BLOCK, &sigmask, NULL) != 0)
+        return NULL;
+
     char* pstat_rawstr = NULL;
     reader_args_t* args = (reader_args_t*)arg;
 
     while(!args->done) {
-        read_procstat(&pstat_rawstr);
+        if(read_procstat(&pstat_rawstr) == PSTAT_FAILED)
+            continue;
         
-        while(queue_push(&raw_pstat_fifo, (void*)pstat_rawstr, strlen((const char*)pstat_rawstr)) != QUEUE_SUCCESS);
+        while(queue_push(&raw_pstat_fifo, (void*)pstat_rawstr, strlen((const char*)pstat_rawstr)) != QUEUE_SUCCESS) {
+            if(args->done)
+                break;
+        }
 
         free(pstat_rawstr);
         pstat_rawstr = NULL;
